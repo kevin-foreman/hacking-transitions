bdc65d54994ff91f3f92ca99a03c4a70df2458dc
alphie1028 <alphie1028@gmail.com>
2023-05-04 14:01:38 -0400

    added map for the cohorts, there is a cors issue

--- client/src/context/LeftColumnContext.jsx
+++ client/src/context/LeftColumnContext.jsx
@@ -3,6 +3,7 @@ import React, {createContext, useEffect, useState} from "react";
 const LeftColumnContext = createContext();
 
 export const LeftColumnProvider = ({children}) => {
+<<<<<<< HEAD
     const [dropDownClicked, setDropDownClicked] = useState("");
     const [cohortClicked, setCohortClicked] = useState("");
     const [cohorts, setCohorts] = useState([]);
@@ -13,6 +14,28 @@ export const LeftColumnProvider = ({children}) => {
     const handleDropClicked = (value, id) =>{
         if(dropDownClicked === value){
             setDropDownClicked("")    
+=======
+    const [dropDownClicked, setDropDownClicked] = useState(false);
+    const [cohortClicked, setCohortClicked] = useState(false);
+    const [addCohortClicked, setAddChohortClicked] = useState(false);
+    const [editCohortClicked, setEditCohortClicked] = useState(false);
+    const [cohorts, setCohorts] = useState([]);
+    const openAddCohort = () => {
+        setAddChohortClicked(true);
+    }
+    const closeAddCohort = () => {
+        setAddChohortClicked(false);
+    }
+    const openEditCohort = () => {
+        setEditCohortClicked(true);
+    }
+    const closeEditCohort = () => {
+        setEditCohortClicked(false);
+    }
+    const handleDropClicked = () =>{
+        if(dropDownClicked){
+            setDropDownClicked(false);
+>>>>>>> 4a33dc3 (added map for the cohorts, there is a cors issue)
         }else{
             setDropDownClicked(value)
             setcohortId(id)
@@ -48,17 +71,37 @@ export const LeftColumnProvider = ({children}) => {
         } else {
             setOpenDropdown(cohortId);
         }
+<<<<<<< HEAD
     };
+=======
+    }
+    useEffect(() => {
+        fetch('http://localhost:8000/api/cohorts')
+            .then(response => response.json())
+            .then(data => setCohorts(data))
+            .catch(error => console.log(error));
+    }, []);
+>>>>>>> 4a33dc3 (added map for the cohorts, there is a cors issue)
 
     return( <LeftColumnContext.Provider value = {{
         dropDownClicked,
         handleDropClicked,
         cohortClicked, 
         handleCohortClicked,
+<<<<<<< HEAD
         cohorts,
         toggleDropDown,
         openDropDown,
         students
+=======
+        addCohortClicked,
+        openAddCohort,
+        closeAddCohort,
+        editCohortClicked,
+        openEditCohort,
+        closeEditCohort,
+        cohorts
+>>>>>>> 4a33dc3 (added map for the cohorts, there is a cors issue)
     }}>
         {children}
     </LeftColumnContext.Provider>
