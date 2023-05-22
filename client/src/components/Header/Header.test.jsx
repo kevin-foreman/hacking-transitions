922315096bdc54935152503fcf924bb68567830e
EZSteezy <Sdeanjr000@gmail.com>
2023-05-22 15:37:55 -0500

    whoops

--- client/src/components/Header/Header.test.jsx
+++ client/src/components/Header/Header.test.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Header from './Header';
+import React from "react";
+import { render, screen } from '@testing-library/react'
+import { expect, describe, it } from 'vitest'
+import App from '../App/App'
+import LeftColumnContext from '../../context/LeftColumnContext'
 
-describe('Header', () => {
-  it('renders the title', () => {
-    render(<Header />);
-    const titleElement = screen.getByText('Hacking Transitions');
-    expect(titleElement).toBeInTheDocument();
-  });
+describe('App', () => {
+  it('renders the App component correctly', () => {
+    const mockValue = {
+      cohortClicked: '',
+      renderStudent: false,
+      cohorts: []
+    }
 
-  it('renders the Galvanize logo', () => {
-    render(<Header />);
-    const logoElement = screen.getByAltText('Galvanize Logo');
-    expect(logoElement).toBeInTheDocument();
-  });
+    render(
+      <LeftColumnContext.Provider value={mockValue}>
+        <App />
+      </LeftColumnContext.Provider>
+    );
 
-  it('renders the greeting with the username', () => {
-    render(<Header username="John" />);
-    const greetingElement = screen.getByText('Hello, John');
-    expect(greetingElement).toBeInTheDocument();
+    // assuming the Header, Footer, LeftColumn, Register and Login components render some text or a title with their names
+    const headerElement = screen.getby('header');
+    expect(headerElement).to.not.be.null;
   });
 });
